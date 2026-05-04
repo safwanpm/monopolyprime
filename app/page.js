181e4a2538bfa4830abfab3f81a2bac06766e81a
safwanpm <safwanpm0@gmail.com>
2026-05-04 18:25:51 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
       <PartnerAlliance />
       <MonopolyReviews />
       <Testimonials />
-      {/* <CTA/>/ */}
+      
       <Contact />
       <Footer />
       <SignaturePopup />
